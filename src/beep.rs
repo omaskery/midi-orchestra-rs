658d58f244fe9aef90ba20f739b6826d8d29b0d9
Oliver Maskery <omaskery@googlemail.com>
2018-04-12 22:05:38 +0100

    made everything square waves because yesssssssssss
    square waves make everything better

--- src/beep.rs
+++ src/beep.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 use pitch_calc::Hz;
 use rodio::Source;
 use rodio;
+use std;
 
 pub struct Beeper {
     endpoint: rodio::Endpoint,
@@ -12,14 +13,67 @@ impl Beeper {
         let endpoint = rodio::default_endpoint().unwrap();
 
         Self {
-            endpoint
+            endpoint,
         }
     }
 
     pub fn beep<H: Into<Hz>>(&self, frequency: H, duration: Duration, volume: f32) {
         let frequency = frequency.into().0;
-        let source = rodio::source::SineWave::new(frequency as u32);
+        let source = SquareWave::new(frequency as u32);
         let source = source.amplify(volume).repeat_infinite().take_duration(duration);
         rodio::play_raw(&self.endpoint, source);
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct SquareWave {
+    freq: f32,
+    num_sample: usize,
+}
+
+impl SquareWave {
+    /// The frequency of the sine.
+    #[inline]
+    pub fn new(freq: u32) -> Self {
+        Self {
+            freq: freq as f32,
+            num_sample: 0,
+        }
+    }
+}
+
+impl Iterator for SquareWave {
+    type Item = f32;
+
+    #[inline]
+    fn next(&mut self) -> Option<f32> {
+        self.num_sample = self.num_sample.wrapping_add(1);
+
+        let value = 2.0 * std::f32::consts::PI * self.freq * self.num_sample as f32 / 48000.0;
+        let sin = value.sin();
+
+        Some(if sin >= 0.0 { 1.0 } else { -1.0 })
+    }
+}
+
+impl Source for SquareWave {
+    #[inline]
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    #[inline]
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    #[inline]
+    fn samples_rate(&self) -> u32 {
+        48000
+    }
+
+    #[inline]
+    fn total_duration(&self) -> Option<Duration> {
+        None
     }
 }
